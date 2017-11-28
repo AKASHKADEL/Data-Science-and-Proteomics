f374458c364937bc53c10f036472b2b4d3db42e7
AKASHKADEL <akashkadel@live.com>
2017-11-28 11:37:55 -0500

    Adding files for running it in PRINCE HPC

---
+++ GRU Implementation/run-main_model.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=capstone_job
+#SBATCH --gres=gpu:1
+#SBATCH --time=50:00:00
+#SBATCH --mem=90GB
+#SBATCH --output=slurm_%A.out
+#SBATCH --error=slurm_%A.err
+
+module purge
+module load python3/intel/3.5.3
+module load pytorch/python3.6/0.2.0_3
+module load torchvision/python3.5/0.1.9
+
+# python3 -m pip install comet_ml --user
+
+python3 -u main_model_gru.py --n_epochs 5 --learning_rate 0.001 --main_data_dir "/scratch/ak6201/Capstone/data/" --out_dir "/scratch/ak6201/Capstone/ModelOutputs/"
